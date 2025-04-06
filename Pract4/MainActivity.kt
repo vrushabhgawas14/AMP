ddcda07456ef4d74d62ebfdf4059d84885331330
Vrushabh <vrushabhgawas14@gmail.com>
2025-04-06 15:27:55 +0530

    Layout And Designing

--- Pract4/MainActivity.kt
+++ Pract4/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.practical1
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ListView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -63,6 +65,48 @@ class MainActivity : AppCompatActivity() {
         val table = findViewById<Button>(R.id.btnTableLayout)
         table.setOnClickListener{
             setContentView(R.layout.table_layout)
+
+            val homeButton = findViewById<Button>(R.id.btnHome)
+            homeButton.setOnClickListener{
+                setContentView(R.layout.activity_main)
+            }
+
+        }
+
+        // Frame Layout
+        val frame = findViewById<Button>(R.id.btnFrameLayout)
+        frame.setOnClickListener{
+            setContentView(R.layout.frame_layout)
+
+            val homeButton = findViewById<Button>(R.id.btnHome)
+            homeButton.setOnClickListener{
+                setContentView(R.layout.activity_main)
+            }
+
+        }
+
+
+        // List Layout
+
+        val listLayout = findViewById<Button>(R.id.btnListLayout)
+        listLayout.setOnClickListener{
+            setContentView(R.layout.list_layout)
+
+            val listView = findViewById<ListView>(R.id.userlist)
+            val items = arrayOf("Apple", "Banana", "Cherry", "Date")
+            val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1,items)
+            listView.adapter = adapter
+
+            val homeButton = findViewById<Button>(R.id.btnHome)
+            homeButton.setOnClickListener{
+                setContentView(R.layout.activity_main)
+            }
+        }
+
+        // Grid Layout
+        val grid = findViewById<Button>(R.id.btnGridLayout)
+        grid.setOnClickListener{
+            setContentView(R.layout.grid_layout)
 
             val homeButton = findViewById<Button>(R.id.btnHome)
             homeButton.setOnClickListener{
